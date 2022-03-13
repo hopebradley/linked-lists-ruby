e304f3e01bd0699c744597b7835ee971a459aa17
Hope Bradley <hope.e.bradley@gmail.com>
2022-03-13 00:58:44 -0500

    wip

--- node.rb
+++ node.rb
@@ -1,2 +1,13 @@
 class Node
+    attr_accessor :value, :next_node
+
+    def initialize(value, next_node=nil)
+        @value = value
+        @next_node = next_node
+    end
+
+    def delete_node(first_node, node_to_delete)
+        
+    end
+
 end
